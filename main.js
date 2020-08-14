33f0b621851833a3be6441af99059c1b36e1a177
Adam <adamjcoonen@gmail.com>
2020-08-14 03:26:57 -0400

    update8.0

--- main.js
+++ main.js
@@ -3,14 +3,16 @@
 
 const holes = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 const gameTime = 30;
-const molePic = '<img class="mole" src="https://media.istockphoto.com/vectors/cartoon-cute-mole-vector-id586064082">'
+const molePic = 'url("https://media.istockphoto.com/vectors/cartoon-cute-mole-vector-id586064082")'
 
 
 /* --------- apps state variables ---------*/
 
 
-let playerScore, molesHit, molesAppeared, molesEsc, plyHitPercent, timestarted = false, counter;
-let seconds = 30
+let playerScore, plyHitPercent, timestarted = false, counter;
+let seconds = 30;
+let hits = 0;
+let appears = 0;
 //These are the numbers that will render on the DOM showing the overa;;
 //success of the player.
 
@@ -20,53 +22,57 @@ let seconds = 30
 
 // These are the DOM elements that render 
 
-const scoreELs = {
-    mHits: document.querySelector('#moles_Hit'),
-    mAppear: document.querySelector('#moles_shown'),
-    mEsc: document.querySelector('#moles_esc')
-}
+
+let mHits = document.getElementById('moles_Hit')
+let mAppear = document.getElementById('moles_shown')
+let mEsc = document.getElementById('moles_esc')
 
 let moleEl = document.querySelector('.mole')
-let holeEl = document.querySelectorAll('.hole')
+let holeEl = document.getElementsByClassName('hole')
 let clockEl = document.getElementById('timer')
+let winMsg = document.getElementById('scoring')
+
+
 
 /* -------- event listeners ---------*/
 
 document.getElementById("container").style.cursor = "crosshair";
 document.querySelector('button').addEventListener('click',startTime );
+document.getElementById("container").addEventListener('click', moleHit)
+document.getElementById('reset').addEventListener('click', init)
 
-// holeEl.addEventListener('click',moleHit)
 
 /* ---------- Functions --------------*/
- function init(){
+
+function init(){ 
+    for (i = 0; i < holeEl.length; i++){
+        holeEl[i].style.backgroundImage = null
+    }
+     timestarted = false
+     seconds = gameTime;
  }
 // This function starts the clock and sets the 'time started to true' as 
 // a condition for the other fucntions to run
     function startTime(){
-        if( timestarted === false){
+        if( seconds > 0 && timestarted === false ){
         timestarted = true;
         counter = setInterval(function(){
             if(seconds >= 0){
                 clockEl.innerHTML = seconds;
                 seconds--;
             }else{
-            clearInterval(counter);
             timestarted = false;
-            seconds = 0
-            init
+            showScore()
             }
             },1000)
-        }  
+             } 
     }
-  
-  
+
     nextMrender(); 
-    
+    nextUnrender();
+
  // get a random time for the moles to apprear
  
- 
-
-
  function mTime(min,max){
     return Math.round(Math.random() * (max - min) + min);
  }
@@ -78,8 +84,18 @@ document.querySelector('button').addEventListener('click',startTime );
  }
 
 
-// This function adds the number of moles clicked by the player to thier score
-
+// This function adds the number of moles clicked by the player to their score and removes the mole element
+function moleHit(evt){ 
+    if(evt.target.style.backgroundImage == molePic && timestarted === true) {
+    evt.target.style.backgroundImage = null
+     hits++ } 
+    }
+// renders the final score for the player
+function showScore(){
+    mHits.innerHTML = `You got ${hits} moles`
+    mAppear.innerHTML = `Out of ${appears}`
+    mEsc.innerHTML = `but ${appears-hits} moles escaped!`
+}
 
 
 
@@ -87,11 +103,26 @@ document.querySelector('button').addEventListener('click',startTime );
 function mRender(cb){
     if (timestarted === true){
        const hole = randHoles(holeEl)
-       hole.innerHTML = molePic 
+       hole.style.backgroundImage = molePic;
+       appears++;
         }
         
-        setTimeout(cb, mTime(500,1000))}
-    function nextMrender(){
-        mRender(nextMrender)  
+        setTimeout(cb, mTime(100,1000))
     }
-console.log('hello world')
+    function mUnRender(cb) {
+        if (timestarted === true ){
+            let full = randHoles(holeEl)
+            full.style.backgroundImage = null;   
+        }
+            
+             setTimeout(cb, 500)
+         }
+    
+    
+        
+    function nextMrender(){
+        mRender(nextMrender)};
+    function nextUnrender(){
+        mUnRender(nextUnrender);
+        };
+        
